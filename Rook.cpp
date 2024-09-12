ae7a04bed54ea96934d064a26b1e92227e7e6f6f
Divyanshi Solanki <161625343+Divyanshi-solanki@users.noreply.github.com>
2024-09-12 21:08:20 +0530

    Add files via upload

---
+++ Rook.cpp
@@ -0,0 +1,18 @@
+/*Problem 1
+
+The rook[hathi] is a piece in the game of chess. During its turn, it may move any non-zero number of squares horizontally or vertically.  Every position on chess board is given by column name followed by row name. The rook position on given chess board is e4. 
+Your task is to find number of possible moves for a rook on an empty chessboard  when position of rook is given as input.*/
+
+//Solution
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"14";
+    return 0;
+}
+
+
